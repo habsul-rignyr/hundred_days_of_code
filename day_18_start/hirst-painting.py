6f9ce115f228f6525c52a7d107c8f9a620648184
habsul <joshamy@gmail.com>
2022-06-03 17:57:54 +0800

    Add colorgram and randomly choose colors

--- day_18_start/hirst-painting.py
+++ day_18_start/hirst-painting.py
@@ -1,1 +1,51 @@
+from turtle import Turtle, Screen
 import colorgram
+import random
+
+pic_colors = colorgram.extract('image.jpg', 25)
+
+tim = Turtle()
+tim.speed(10)
+scr = Screen()
+scr.colormode(255)
+
+muh_tuple = ()
+for i in range(len(pic_colors)):
+    choice = random.choice(pic_colors)
+    red = choice.rgb.r
+    green = choice.rgb.g
+    blue = choice.rgb.b
+    muh_tuple = (red, green, blue)
+
+
+    tim.fillcolor(muh_tuple)
+    tim.pencolor(muh_tuple)
+    tim.penup()
+    tim.forward(30)
+    tim.pendown()
+    tim.begin_fill()
+    tim.circle(10)
+    tim.end_fill()
+
+
+
+
+
+
+
+
+
+
+
+scr.exitonclick()
+
+
+
+
+
+
+
+
+
+
+
